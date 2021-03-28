38fe673866021db8d8cf6b7b07114206aecbbd69
Kiryakova Stela <S.Kiryakova@printecgroup.com>
2021-03-28 18:50:24 +0300

    add form styling and error handling

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,9 +1,20 @@
 import style from './styles.module.css';
 
+import { Link } from 'react-router-dom';
+
 const ErrorPage = () => {
-    return(
-        <h1>Error Page</h1>
-    );
+    return (
+        <section>
+
+            <div>
+                <h2>404 - Not Found</h2>
+                <Link className="button" to="/products">Go to Products</Link>
+            </div>
+
+            <img src="https://res.cloudinary.com/stela-cloud/image/upload/o7hfrt963.png" alt="404" />
+        </section>
+    )
 }
 
 export default ErrorPage;
+
--- src/components/ProductAdd/index.js
+++ src/components/ProductAdd/index.js
@@ -10,33 +10,43 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 const ProductAdd = ({
-    match,
     history
 }) => {
     const [name, setName] = useState('');
+    const [file, setFile] = useState();
+    /*
+    const [state, setState] = useState({});
     const [category, setCategory] = useState('');
     const [description, setDescription] = useState('');
     const [file, setFile] = useState();
-    const [price, setPrice] = useState();
-    const [errors, setErrors] = useState({name: ''});
+    const [price, setPrice] = useState();*/
+    const [errors, setErrors] = useState({name: '', file: null});
 
     const onSubmitHandler = async (e) => {
         e.preventDefault();
 
-        if (e.target.name.value.length < 5) {
-            setErrors({ ...errors, name: 'Product name should be at least 5 characters long!'});
+        setErrors({});
+
+        //if (state.name.length < 5) {
+        if (name.length < 5) {console.log("aaa");
+            setErrors({name: 'Product name should be at least 5 characters long!'});
+            //setErrors({ ...errors, name: 'Product name should be at least 5 characters long!'});
+            return;
         }
+
         console.log(errors);
 
-        if (e.target.price.value.length < 0) {
+        /*if (state.price.length < 0) {
             setErrors({ ...errors, price: 'Product price should be positive value!'});
-        }
+        }*/
 
+        //if (!state.file) {
         if (!file) {
-            setErrors({ ...errors, image: 'Product image should be set!'});
+            setErrors({file: 'Product image should be set!'});
+            return;
         }
-        console.log("aaa");
-        if(!errors){
+        
+        if(errors != {}){
             const { name, price, description, category } = e.target;
 
             let formData = new FormData();
@@ -65,17 +75,22 @@ const ProductAdd = ({
     };
 
     const onChangeHandler = (e) => {
+        /*const { name, value } = e.target;
+        console.log(name);
+        setState({ ...state, [name]: value });
+console.log(state);*/
         setName(e.target.value);
     }
 
     const onChangeHandlerFile = (e) => {
+        //setState({ ...state, file: e.target.files[0] });
         setFile(e.target.files[0]);
     }
 
     return (
         <div className={style['product-add-container']}>
                 <form className={style['product-add-form']} onSubmit={onSubmitHandler}>
-                    <label htmlFor="name">Product name</label>
+                    <label className={style['label']} htmlFor="name">Product name</label>
                     <input className={style['input-field']}  
                         type="text" 
                         id="name" 
@@ -89,21 +104,18 @@ const ProductAdd = ({
                     }
 
 
-                    <label htmlFor="price">Price</label>
-                    <input
+                    <label className={style['label']} htmlFor="price">Price</label>
+                    <input className={style['input-field']} 
                         type="number"
                         id="price"
-                        name="price"
+                        name="price" 
                     />
-                    {errors.price && 
-                        <span>{errors.price}</span>
-                    }
 
-                    <label htmlFor="description">Description</label>
-                    <textarea name="description" />
+                    <label className={style['label']} htmlFor="description">Description</label>
+                    <textarea className={style['input-field']} name="description" />
 
-                    <label htmlFor="category"></label>
-                    <select
+                    <label className={style['label']} htmlFor="category">Category</label>
+                    <select className={style['input-field']} 
                         name="category"
                         id="category" 
                     >
@@ -112,14 +124,21 @@ const ProductAdd = ({
                         )}
                     </select>
 
-                    <label htmlFor="upload-file">Upload File</label>
-                    <input id="upload-file" type="file" className="upload-file-button" onChange={onChangeHandlerFile} />
+                    <label className={style['label']} htmlFor="upload-file">Upload File</label>
+                    <input className={style['input-field']} 
+                        id="upload-file" 
+                        name="file" 
+                        type="file" 
+                        className="upload-file-button" 
+                        onChange={onChangeHandlerFile} 
+                    />
                     {errors.file && 
                         <span>{errors.file}</span>
                     }
 
                     <div className={style['button-wrapper']}>
-                        <input type="submit" value="Send"  />
+                        <input type="submit" value="Save"  />
+                        <Link to="/products">Cancel</Link>
                     </div>
                 </form>
             </div>
